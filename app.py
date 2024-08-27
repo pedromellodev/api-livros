4a7d04952ac4c53b26cedc985cf5598b37dc5d28
pedromellodev <pedromello19122006@gmail.com>
2024-08-27 11:47:07 -0300

    Criando a função "Editar Livro por Id"

--- app.py
+++ app.py
@@ -32,6 +32,14 @@ def consultar_livro_id(id):
         if livro.get('id') == id:
             return jsonify(livro)
 # Editar
+@app.route('livros/<int:id>', methods=['PUT'])
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+
+    for indice,livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
 # Excluir
 
 app.run(port=5000, host='localhost', debug=True)
